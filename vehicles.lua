c9928aa11d31c17d9959f773222765401c3c32c3
Mahluk Akıllıok <106095586+youngmahlukde@users.noreply.github.com>
2022-05-23 21:29:28 +0300

    Update vehicles.lua

--- vehicles.lua
+++ vehicles.lua
@@ -5,4 +5,5 @@ carshopCars = {
     {'Subaru', 'Griffith', 429, 278, 67550, 75},
     {'Toyota', 'Camry', 546, 304, 6600, 10},
     {'Kawasaki', 'Ninja ZX8R', 522, 805, 115000, 250},
+    {'Range', 'Ninja ZX8R', 130, 205, 135000, 210},
 }
